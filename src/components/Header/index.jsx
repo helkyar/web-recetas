b1f7acc947b7e42e69069ce0c60aa93dfae04a87
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-01 23:23:05 +0100

    Add basic style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,22 @@
 import './component.css';
 
+import SearchBar from './SearchBar';
+import logo from './img/logo.png'
+import facebook from './img/facebook.svg'
+import twitter from './img/twitter.svg'
+import insta from './img/insta.svg'
+
 function Header() {
   return (
-    <h1>Header</h1>
+    <header className='header'>
+      <SearchBar/>
+      <button>New</button>
+      <button>Popu</button>
+      <img className="logo" src={logo}/>
+      <img className="media" src={facebook} />
+      <img className="media" src={insta} />
+      <img className="media" src={twitter} />
+    </header>
   );
 }
 
