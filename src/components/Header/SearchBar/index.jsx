873cdcb60ffac3c76c32c95152b42eb4f81e444f
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-11 18:21:39 +0100

    First try to adding react-links

--- src/components/Header/SearchBar/index.jsx
+++ src/components/Header/SearchBar/index.jsx
@@ -1,9 +1,18 @@
-import './component.css';
+import { Link } from "react-router-dom";
+
+import './component.css'
+import search from './img/search.png'
 
 function SearchBar() {
-  return (
-    <input tipe="text" value="SearchBar"/>
-  );
+    // enter-click listener search
+    // search info is passed throw GET (/string for searches, /number for the rest)
+    // link
+    return (
+        <div className="search-bar-container">
+            <input type="text" placeholder="ingrediente, área..."/>
+            <Link to="/recipes/search/pedo" />
+                {/* <img src={search} alt="search button" /> */}
+        </div>  );
 }
-
+ 
 export default SearchBar;
