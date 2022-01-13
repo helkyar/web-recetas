ae77d2006b387fedb122d16f7b9a70d832f5effa
Borjamrd <borjamrd1@gmail.com>
2022-01-13 21:50:56 +0100

    main meals with images

--- src/components/Main/RecipeHighlights/index.jsx
+++ src/components/Main/RecipeHighlights/index.jsx
@@ -1,41 +1,32 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 
 export function RecipeHighlights(){
-    /* async function loadInfo (){
-
-        const resp = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
-        let meal = resp.data.meal
-        console.log(meal)
-    }
-
-    useEffect(loadInfo,[]);
- */
-
-
+    const [meals, setMeals] = useState([]);
 
 
     let meal = [];
     useEffect(() => {
-
+        
         async function fetchData(){
             const resp = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
-            meal = resp.data.meal
-            console.log(meal)
+            meal = resp.data.meals
+            setMeals(meal)
         }
         fetchData();
     }, []);
-    
+   
+   
    
     
 
-    let hightlights = meal.map((element)=> 
+    let hightlights = meals.map((element)=> 
         <div>
-            <h3>{element.meals.strMeal}</h3>
-            <span>Categoría: </span><p>{element.meals.strCategory}</p>
+            <h3>{element.strMeal}</h3>
+            <span>Categoría: </span><p>{element.strCategory}</p>
             <div>
-                <video src={element.meals.strYoutube} height="100px">
+                <video src={element.strYoutube} height="100px">
                 </video>
             </div>
          </div>
