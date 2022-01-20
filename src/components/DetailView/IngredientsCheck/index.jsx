f6526239ff33b6bb644dc7a7fe34ea950bd0d5cc
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-20 20:28:54 +0100

    Fix useState slow execution

--- src/components/DetailView/IngredientsCheck/index.jsx
+++ src/components/DetailView/IngredientsCheck/index.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function IngrdientsCheck() {
+  const totalingredients = 21;
   const url = "http://www.themealdb.com/api/json/v1/1/lookup.php?i=";
   let resp;
   let elements = [];
@@ -17,26 +18,27 @@ function IngrdientsCheck() {
 
   useEffect(()=>{
     async function fetchRecipe(){
-      resp = await axios.get(url+params.id); 
+
+      resp = await axios.get(`${url}${params.id}`); 
       setRecipe(resp.data.meals[0]);
       console.log(recipe);
       //Loop harcored to 21 because of api
-      for(let i = 1; i < 21; i++){
-        let ingr = recipe[`strIngredient${i}`];
+      const response = resp.data.meals[0];
+      for(let i = 1; i < totalingredients; i++){
+        
+        let ingr = response[`strIngredient${i}`];
         if (ingr == "" || ingr == undefined ) {break;}
         // Grap the measures and separate its numbers 
-        elements.push([ingr, recipe[`strMeasure${i}`]]);
+        elements.push([ingr, response[`strMeasure${i}`]]);
       }
       setingredients(elements);
-      console.log(ingredients)
-      console.log(elements)
     }
     fetchRecipe();
   },[])
 
   return (
     <>
-      {recipe!=[] && 
+      {recipe!=[] &&
       <div className='recipe'>
         <h1>{recipe.strMeal || 'Error con fetch'}</h1>
         <p>Personas: 
@@ -48,12 +50,11 @@ function IngrdientsCheck() {
           {ingredients.map((ingredient, i)=>
             <div key={`check${i}`} className='check'>
               <input id={`check${i}`} type="checkbox"/>
-              <label for={`check${i}`}> 
+              <label htmlFor={`check${i}`}> 
                 {`${ingredient[0]} (${ingredient[1]} ${people > 1 ? 'x'+people:''})`} 
               </label>
             </div>
           )}
-          {console.log(recipe)}
         </section>
       </div>}
     </>);
