70a29f65428f3f27d2550a6fd7752b858c0e6c59
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-20 12:30:20 +0100

    Add measures modifier

--- src/components/DetailView/IngredientsCheck/index.jsx
+++ src/components/DetailView/IngredientsCheck/index.jsx
@@ -10,6 +10,7 @@ function IngrdientsCheck() {
   let elements = [];
 
   const navigate = useNavigate();
+  const [people, setPeople] = useState(1)
   const [recipe, setRecipe] = useState([]);
   const [ingredients, setingredients] = useState([]);
   let params = useParams();
@@ -23,7 +24,7 @@ function IngrdientsCheck() {
       for(let i = 1; i < 21; i++){
         let ingr = recipe[`strIngredient${i}`];
         if (ingr == "" || ingr == undefined ) {break;}
-
+        // Grap the measures and separate its numbers 
         elements.push([ingr, recipe[`strMeasure${i}`]]);
       }
       setingredients(elements);
@@ -33,20 +34,23 @@ function IngrdientsCheck() {
     fetchRecipe();
   },[])
 
-
-
   return (
     <>
       {recipe!=[] && 
       <div className='recipe'>
         <h1>{recipe.strMeal || 'Error con fetch'}</h1>
-        <p>pio pio</p>
-        <section>
+        <p>Personas: 
+          <button onClick={()=>setPeople((p)=>p-=1)}>-</button> 
+          {people >= 1 ? people : setPeople(1)} 
+          <button onClick={()=>setPeople((p)=>p+=1)}>+</button></p>
+        <section className='checklist'>
           {/* Save recipe changes, +/- buttons to ingredients, change ingredients */}
           {ingredients.map((ingredient, i)=>
             <div key={`check${i}`} className='check'>
               <input id={`check${i}`} type="checkbox"/>
-              <label for={`check${i}`}> {`${ingredient[0]} ${ingredient[1]}`} </label>
+              <label for={`check${i}`}> 
+                {`${ingredient[0]} (${ingredient[1]} ${people > 1 ? 'x'+people:''})`} 
+              </label>
             </div>
           )}
           {console.log(recipe)}
