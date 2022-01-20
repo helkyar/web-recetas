4e5ab5eca3bf93aa4dfd9bcd35540de4a5f600ee
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2022-01-20 12:32:44 +0100

    Revert "Oua hkz mf ingredients check"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes as Switch,
-  Route
-} from "react-router-dom";
-
-import Header from './components/Header';
-import Main from './components/Main'
-import Results from './components/Results';
-import DetailView from './components/DetailView';
-import Footer from './components/Footer';
-
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-        <Switch>
-          <Route path="/" element={<Main />}/>
-          <Route path="/recipes/:type/:search" element={<Results />}/>
-          <Route path="/recipe/:id" element={<DetailView/>}/>
-        </Switch>
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import './component.css';
-
-function Footer() {
-  return (
-    <h1>Footer</h1>
-  );
-}
-
-export default Footer;
