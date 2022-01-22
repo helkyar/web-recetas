da21a0e611dd45f8bb9338ac750b29fc5a95b50c
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-22 14:02:45 +0100

    Component restructuring

--- src/components/DetailView/IngredientsCheck/index.jsx
+++ src/components/DetailView/IngredientsCheck/index.jsx
@@ -1,30 +1,16 @@
 import './component.css';
-
-import { useParams,useNavigate, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 
-function IngrdientsCheck() {
+import People from './People';
+
+function IngrdientsCheck({recipe}) {
   const totalingredients = 21;
-  const url = "http://www.themealdb.com/api/json/v1/1/lookup.php?i=";
-  let resp;
   let elements = [];
 
   const navigate = useNavigate();
   const [people, setPeople] = useState(1)
-  const [recipe, setRecipe] = useState([]);
   const [ingredients, setingredients] = useState([]);
-  let params = useParams();
-
-  useEffect(()=>{
-    async function fetchRecipe(){
-
-      resp = await axios.get(`${url}${params.id}`); 
-      setRecipe(resp.data.meals[0]);
-      console.log(recipe);
-    }
-    fetchRecipe();
-  },[])
 
   useEffect(()=>{
     for(let i = 1; i < totalingredients; i++){
@@ -34,11 +20,10 @@ function IngrdientsCheck() {
 
       //Separate numbers from letters and multiply by {people}
       let measures = recipe[`strMeasure${i}`];
-
       let quantities = measures.split('/');
       let newMeasures = quantities.map((quantitie)=>{
         let numb = quantitie.match(/[0-9.,]+/g); 
-        return quantitie.replace(/[0-9.,]+/g, parseFloat(numb)*people)
+        return quantitie.replace(/[0-9.,]+/g, (parseFloat(numb)*people).toFixed(1))
       })
 
       //Put it all together
@@ -50,12 +35,8 @@ function IngrdientsCheck() {
   return (
     <>
       {recipe!=[] &&
-      <div className='recipe'>
-        <h1>{recipe.strMeal || 'Error con fetch'}</h1>
-        <p>Personas: 
-          <button onClick={()=>setPeople((p)=>p-=1)}>-</button> 
-          {people >= 1 ? people : setPeople(1)} 
-          <button onClick={()=>setPeople((p)=>p+=1)}>+</button></p>
+      <div className='ingredients'>
+        <People setpeople={{people, setPeople}}/>
         <section className='checklist'>
           {/* Save recipe changes, +/- buttons to ingredients, change ingredients */}
           {ingredients.map((ingredient, i)=>
