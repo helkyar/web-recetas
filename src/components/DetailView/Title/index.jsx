aee07c00d057c1bd6ec9f5f91def3a7e28b54059
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-25 21:53:08 +0100

    Centering the fucking logo once and for all

--- src/components/DetailView/Title/index.jsx
+++ src/components/DetailView/Title/index.jsx
@@ -11,7 +11,8 @@ function Title({recipe}) {
       <div className='recipe-title'>
         <h1>{recipe.strMeal || '404 Recipe not Found'}</h1>
         <img src={recipe.strMealThumb} alt={`foto de ${recipe.strMeal}`} />
-        {recipe.strTags ? recipe.strTags.split(',') : ''}
+        {recipe.strTags && recipe.strTags.split(',').map((tag, id)=>
+          <Link key={`tag${id}`} to={`/recipes/tag/${tag}`}> {tag} </Link>)}
       </div>}
     </>);
 }
