adcc7d56edc422a4b5e633e4476956ba643c7ebc
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-27 21:43:10 +0100

    Set numbers to instructions

--- src/components/DetailView/Instructions/index.jsx
+++ src/components/DetailView/Instructions/index.jsx
@@ -11,8 +11,8 @@ function Instructions({recipe}) {
       <div className='recipe-instructions'>
         <h1>Instrucciones</h1>
         <iframe width="427" height="240" src={recipe.strYoutube.split('watch?v=').join('embed/')} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
-        {recipe.strInstructions.replace(/[()]/g,'').split('.').map((instruction)=>
-        <p>{instruction}</p>)}
+        {recipe.strInstructions.replace(/[()]/g,'').split('.').map((instruction, i)=>
+        <p>{instruction ? `${i+1}. ${instruction}`: ''}</p>)}
       </div>}
     </>);
 }
