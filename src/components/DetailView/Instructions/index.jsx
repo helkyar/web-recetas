07599af71262084543cd8c4dbf4fca1c7da1799f
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2022-01-27 21:26:54 +0100

    add text instructions

--- src/components/DetailView/Instructions/index.jsx
+++ src/components/DetailView/Instructions/index.jsx
@@ -7,12 +7,13 @@ function Instructions({recipe}) {
   
   return (
     <>
+      {recipe != undefined && 
       <div className='recipe-instructions'>
         <h1>Instrucciones</h1>
-        {/* <iframe width="427" height="240" src="https://www.youtube.com/embed/VVnZd8A84z4"            title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
-        {recipe.strYoutube != undefined &&       
-        <iframe width="427" height="240" src={recipe.strYoutube.split('watch?v=').join('embed/')} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>}
-        {recipe.strYoutube != undefined && console.log(recipe.strYoutube.split('watch?v=').join('embed/'))}      </div>
+        <iframe width="427" height="240" src={recipe.strYoutube.split('watch?v=').join('embed/')} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+        {recipe.strInstructions.replace(/[()]/g,'').split('.').map((instruction)=>
+        <p>{instruction}</p>)}
+      </div>}
     </>);
 }
 
